66b6ee5495766b83375626d904e4311ddb631122
Sindarapos <sindarapos@gmail.com>
2019-02-20 11:09:38 +0100

    Expand typography

--- client/src/components/common/typography/Body.js
+++ client/src/components/common/typography/Body.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import ExtendWithFont from "./ExtendWithFont";
+import PropTypes from 'prop-types';
+import ExtendWithFont from './ExtendWithFont';
 
 const StyledBody = styled.p`
-  font-size: 1em;
+  ${props => `
+    margin-top: 0;
+    font-size:
+    ${(props.variant === 'normal' && '1em')
+    || (props.variant === 'small' && '0.8125em')
+    || (props.variant === 'large' && '1.2em')};
+  `}
 `;
 
 const Body = (props) => {
-  const { children } = props;
+  const { variant, children } = props;
   return (
     <ExtendWithFont>
-      <StyledBody>{children}</StyledBody>
+      <StyledBody variant={variant}>{children}</StyledBody>
     </ExtendWithFont>
   );
 };
 
+Body.propTypes = {
+  children: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+};
+
+Body.defaultProps = {
+  variant: 'normal',
+};
 
 export default Body;
--- client/src/components/common/typography/index.js
+++ client/src/components/common/typography/index.js
@@ -1,9 +1,11 @@
 import Title from './Title';
 import Body from './Body';
 import ExtendWithFont from './ExtendWithFont';
+import Caption from './Caption';
 
 export {
   Title,
   Body,
+  Caption,
   ExtendWithFont,
 };
