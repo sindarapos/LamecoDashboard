8e23f4f5bfbd21b62520644f52f9e831fc124185
Sindarapos <sindarapos@gmail.com>
2019-02-20 14:59:42 +0100

    Export heading

--- client/src/components/common/typography/index.js
+++ client/src/components/common/typography/index.js
@@ -2,10 +2,12 @@ import Title from './Title';
 import Body from './Body';
 import ExtendWithFont from './ExtendWithFont';
 import Caption from './Caption';
+import Heading from './Heading';
 
 export {
   Title,
   Body,
   Caption,
+  Heading,
   ExtendWithFont,
 };
