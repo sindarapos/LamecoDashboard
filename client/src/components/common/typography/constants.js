00a4956e7fb00037cd3593b1744cb365ed40981f
Sindarapos <sindarapos@gmail.com>
2019-03-04 16:13:07 +0100

    Font coloring constants

--- client/src/components/common/typography/constants.js
+++ client/src/components/common/typography/constants.js
@@ -1,9 +1,15 @@
 const FONT_FAMILY = '"Roboto", "Helvetica", "Arial", sans-serif';
 const FONT_STYLE = 'normal';
 const FONT_WEIGHT = 400;
+const FONT_COLOR = {
+  BLUE: '#3155A4',
+  BLACK: '#1d1d1d',
+  ORANGE: '#E1865E',
+};
 
 export {
   FONT_FAMILY,
   FONT_STYLE,
-  FONT_WEIGHT
+  FONT_WEIGHT,
+  FONT_COLOR,
 };
