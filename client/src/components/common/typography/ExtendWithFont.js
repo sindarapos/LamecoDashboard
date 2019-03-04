aaf22b12f9088c885c86c0d594f2efeb3649994f
Sindarapos <sindarapos@gmail.com>
2019-03-04 16:14:01 +0100

    Allow for color and block margin change

--- client/src/components/common/typography/ExtendWithFont.js
+++ client/src/components/common/typography/ExtendWithFont.js
@@ -5,6 +5,7 @@ import {
   FONT_FAMILY,
   FONT_STYLE,
   FONT_WEIGHT,
+  FONT_COLOR,
 } from './constants';
 
 const WithFont = styled.div`
@@ -12,13 +13,24 @@ const WithFont = styled.div`
   font-family: ${FONT_FAMILY};
   font-style: ${FONT_STYLE};
   font-weight: ${FONT_WEIGHT};
+  ${props => `
+    color: ${props.color};
+    ${props.disableMarginBlockStart && '> * {margin-block-start: 0;}'}
+    ${props.disableMarginBlockEnd && '> * {margin-block-end: 0;}'}
+  `}
 `;
 
 const ExtendWithFont = (props) => {
-  const { children } = props;
+  const {
+    children, color, disableMarginBlockEnd, disableMarginBlockStart,
+  } = props;
   return (
     React.Children.map(children, child => (
-      <WithFont>
+      <WithFont
+        color={color}
+        disableMarginBlockEnd={disableMarginBlockEnd}
+        disableMarginBlockStart={disableMarginBlockStart}
+      >
         {child}
       </WithFont>
     ))
@@ -29,6 +41,15 @@ ExtendWithFont.proptypes = {
   children: PropTypes.arrayOf(
     PropTypes.oneOfType([PropTypes.string, PropTypes.func, PropTypes.object]),
   ),
+  color: PropTypes.string,
+  disableMarginBlockEnd: PropTypes.bool,
+  disableMarginBlockStart: PropTypes.bool,
+};
+
+ExtendWithFont.defaultProps = {
+  color: FONT_COLOR.BLACK,
+  disableMarginBlockEnd: true,
+  disableMarginBlockStart: true,
 };
 
 export default ExtendWithFont;
